ce7e72c4b0a47e1b9d505d665fdd96cd5f0bd086
Konstantin Makarchev <kostya27@gmail.com>
2015-04-25 00:51:54 +0300

    fix logger

--- lib/jober/logger.rb
+++ lib/jober/logger.rb
@@ -14,7 +14,11 @@ module Jober::Logger
 
     class_eval <<-Q
       def #{method_name}(msg = nil, &block)
-        logger.send(:#{method_name}, "[\#{self.class.to_s}] \#{msg}", &block)
+        if block
+          logger.send(:#{method_name}) { "[\#{self.class.to_s}] \#{block.call}" }
+        else
+          logger.send(:#{method_name}, "[\#{self.class.to_s}] \#{msg}", &block)
+        end
       end
     Q
   end
